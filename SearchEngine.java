36f6f5ed751f9c4538040942951cf084411c8676
Valval144 <vsanchezmartinez@ucsd.edu>
2022-10-14 20:24:16 -0700

    edit searchEngine

--- SearchEngine.java
+++ SearchEngine.java
@@ -4,39 +4,43 @@ import java.util.ArrayList;
 import java.util.List;
 
 class Handler implements URLHandler {
-    // The one bit of state on the server: a number that will be manipulated by
-    // various requests.
-    int num = 0;
-    List<String> fruitList = new ArrayList<String>();
-    //[pineapple, apple]
+    ArrayList<String> fruitList = new ArrayList<String>();
 
     public String handleRequest(URI url) {
+
         if (url.getPath().equals("/")) {
-            return String.format("Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
+            return String.format("List of Fruits:" + fruitList);
         } else {
             System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                //this finds the question mark 
 
-                // /add?s=apple
-                // paramters[] = [s, apple]
-                //paramters[1]
-                //fruitlist.add parameters 1 
-                String[] parameters = url.getQuery().split("s=");
+            if (url.getPath().contains("/add")) {
+                fruitList.add(url.getQuery().split("s=")[1]);
+                return String.format("Object added!");
+            }
+
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                ArrayList<String> search_results = new ArrayList<>();
                 if (parameters[0].equals("s")) {
-                    num += Integer.parseInt(fruitList.add());
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
+                    for (String fruit: fruitList) {
+                        if (fruit.contains(parameters[1])) {
+                            search_results.add(fruit);
+                            return search_results.toString();
+                        }
+                    }
                 }
+                //return String.format("Object added!", parameters[1], search_results);
+                return fruitList.toString();
+                
+
+
             }
             return "404 Not Found!";
         }
     }
 }
 
-class NumberServer {
+class FruitServer {
     public static void main(String[] args) throws IOException {
         if(args.length == 0){
             System.out.println("Missing port number! Try any number between 1024 to 49151");
